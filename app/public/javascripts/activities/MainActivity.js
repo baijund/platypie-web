3fa60494047715fe8c490b684882aea3571ac612
Baijun <bdesai35@gatech.edu>
2016-03-23 23:52:55 -0400

    Replicated main activity and added routing on button clicks

---
+++ app/public/javascripts/activities/MainActivity.js
@@ -0,0 +1,9 @@
+function goToLogin(){
+  console.log("Going to login screen");
+  window.location.href = "/login";
+}
+
+function goToRegister(){
+  console.log("Going to register screen");
+  window.location.href = "/register";
+}
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Platypie' });
+  res.render('index', { title: 'No Chill' });
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'No Chill' });
+});
+
+/* GET register page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'No Chill' });
 });
 
 module.exports = router;
