48f57d9ecf4c93a87727919b8f9410c7dde3afc3
Baijun <bdesai35@gatech.edu>
2016-03-27 12:36:22 -0400

    Added method signatures to users for login, getUserList, getBannedUsers, getUnbannedUsers, and unadmin. Also added route for login

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,8 @@ CONNNECTION_OBJ = {
     ssl: true
 }
 
+var users = require('./users.js');
+
 
 var express = require('express');
 var router = express.Router();
@@ -32,9 +34,45 @@ router.get('/register', function(req, res, next) {
 
 /* POST users/getUser. */
 router.post('/users/getUser', function(req, res, next) {
-  var users = require('./users.js');
-  var profile = users.getUser(req.body.username, res);
+  if (!req.body.username){
+    res.json({invalid: true});
+  } else {
+    users.getUser(req.body.username, res);
+  }
 });
+
+/* POST users/addUser */
+router.post('/users/addUser', function(req, res, next) {
+  var userObjectString = req.body.userObjectString;
+
+  if (!userObjectString){
+    console.log("userObjectString is missing");
+    res.json({invalid: true});
+  } else {
+    var userObject = JSON.parse(userObjectString);
+    //Make sure userObject and all its fields are defined
+    if (!userObject || !userObject.firstName || !userObject.lastName || !userObject.email || !userObject.username || !userObject.password || !userObject.about || !userObject.major){
+      console.log("Something is missing");
+      console.log(userObject.firstName & userObject.lastName );
+      res.json({invalid: true});
+    } else {
+      console.log("Nothing is missing");
+      users.addUser(userObject, res);
+    }
+  }
+});
+
+
+/* POST users/login. */
+router.post('/users/login', function(req, res, next) {
+  //Require username and password fields
+  if (!req.body.username || !req.body.password){
+    res.json({invalid: true});
+  } else {
+    users.login(req.body.username, req.body.password, res);
+  }
+});
+
 
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 
-//Takes in username and response object to generate JSON response of user profile without password or {invalid: true}
+//Takes in username and Response object to generate JSON response of user profile without password or {invalid: true}
 var getUser = function(username, res){
 
   pg.connect(CONNNECTION_OBJ, function(err, client, done) {
@@ -41,9 +41,68 @@ var getUser = function(username, res){
   });
 }
 
+//Takes in a UserObject and Response object and adds the user to the database. A JSON response of {invalid: true} is generated if it failed. A response of {invalid: false} is generated if it succeeds.
+var addUser = function(userObject, res){
+  pg.connect(CONNNECTION_OBJ, function(err, client, done) {
+    if(err) {
+      return console.error('could not connect to postgres', err);
+    }
+
+    //var q = "﻿INSERT INTO public.users VALUES ('" + userObject.firstName + "', '" + userObject.lastName + "', '" + userObject.email + "', '" + userObject.username + "', '" + userObject.password + "', '" + userObject.about + "', '" + userObject.major + "', false, false, false);";
+
+    //q = "INSERT INTO public.users VALUES ('fires', 'last', 'Admin@Admindsa.com', 'admdsasin', 'password', 'I am an admin', 'Physics', false, true, true);";
+
+    var q = "INSERT INTO public.users VALUES ('" + userObject.firstName + "', '" + userObject.lastName + "', '" + userObject.email + "', '" + userObject.username + "', '" + userObject.password + "', '" + userObject.about + "', '" + userObject.major + "', false, false, false);";
+
+    console.log("Query: " + q);
+
+    client.query(q, function(err, result) {
+        if(err) {
+          return console.error('error running query', err);
+        }
+
+        done();
+
+        var row = result.rows[0];
+        //console.log(row);
+        if (!row){
+          res.json({invalid: true});
+        } else {
+          row.invalid = false;
+          res.json(row);
+        }
+      });
+  });
+}
+
+//Takes in String username, String password, and Repsonse res. Renders {error: false} if succeeded and sets session cookies. 
+var login = function(username, password, res){
+
+}
+
+//Takes in a Response object and renders an array of Strings representing the users (incomplete)
+var getUserList = function(res){
+
+}
+
+//Takes in a Response object and renders an array of Strings representing the banned users except superadmin (incomplete)
+var getBannedUsers = function(res){
+
+}
+
+//Takes in a Response object and renders an array of Strings representing the unbanned users except superadmin (incomplete)
+var getUnbannedUSers = function(res){
+
+}
+
+//Takes in a String representing the username of an admin, and a response object. Renders a {error: false} if successful or else a {error: false, errormsg: (SOME MESSAGE)}
+var unadmin = function(username, res){
+
+}
 
 
 
 module.exports = {
-  "getUser": getUser
+  "getUser": getUser,
+  "addUser": addUser
 }
