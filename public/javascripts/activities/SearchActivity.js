f8cc3a621b46bb33edba5c301d5168ac037764cb
Baijun <bdesai35@gatech.edu>
2016-04-26 00:25:24 -0400

    Search page fetches and lists

--- public/javascripts/activities/SearchActivity.js
+++ public/javascripts/activities/SearchActivity.js
@@ -1,19 +1,26 @@
-var s = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=7wj4raxmwrr475d3na6ujxbr&q=frozen"
+var s = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=7wj4raxmwrr475d3na6ujxbr&q="
 $(document).ready(function(){
 
-  $.get(s,function(data){
-    alert(data);
-  })
 
   var movieList = $("#movies");
 
   $("#searchButt").click(function(e){
     e.preventDefault();
+    movieList.html("");
+    $.ajax({
+      url: s+$("#search").val(),
 
-    for(var i = 0; i < 10; i++){
-      movieList.append("<option value='i'>"+i+"</option>");
+      dataType: "jsonp",
+      success: function( response ) {
+        console.log(response)
+        for(var i = 0; i < response.movies.length; i++){
+          movieList.append("<option value='"+response.movies[i].id+"'>"+response.movies[i].title+ " (" + response.movies[i].year + ")</option>");
+        }
 
-    }
+      }
+
+      });
+
 
 
 
